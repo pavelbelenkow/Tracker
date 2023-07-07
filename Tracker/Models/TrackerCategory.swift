c90792d8bc7710bbcea129782fbe8135925da773
Pavel Belenkow <heartofcassiopeia@yandex.ru>
2023-07-08 02:19:16 +0300

    Added models to project

---
+++ Tracker/Models/TrackerCategory.swift
@@ -0,0 +1,13 @@
+//
+//  TrackerCategory.swift
+//  Tracker
+//
+//  Created by Pavel Belenkow on 21.06.2023.
+//
+
+import Foundation
+
+struct TrackerCategory {
+    let title: String
+    let trackers: [Tracker]
+}
