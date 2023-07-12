479eafed198ca77c83ef66c20e677bf4decaf5b9
Pavel Belenkow <heartofcassiopeia@yandex.ru>
2023-07-13 02:32:35 +0300

    Implemented EmojiColorCollectionView with cells and header

---
+++ Tracker/Helpers/EmojiColorCollectionView/SectionHeader.swift
@@ -0,0 +1,41 @@
+//
+//  SectionHeader.swift
+//  Tracker
+//
+//  Created by Pavel Belenkow on 13.07.2023.
+//
+
+import UIKit
+
+// MARK: - SectionHeader Class
+
+final class SectionHeader: UICollectionReusableView {
+    
+    // MARK: - Properties
+    
+    static let reuseIdentifier = "header"
+    
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.TrackerFont.bold19
+        label.textColor = UIColor.TrackerColor.black
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    // MARK: - Methods
+    
+    private func addTitleLabel() {
+        addSubview(titleLabel)
+        
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: topAnchor),
+            titleLabel.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 28)
+        ])
+    }
+    
+    func configure(from title: String) {
+        titleLabel.text = title
+        addTitleLabel()
+    }
+}
