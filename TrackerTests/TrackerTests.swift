2ffa4ae89f05393f3fd34688a10bb8a5d1baa35c
Pavel Belenkow <heartofcassiopeia@yandex.ru>
2023-08-07 23:48:05 +0300

    Attached snapshot testing dependency to the project Added target for testing to the project

---
+++ TrackerTests/TrackerTests.swift
@@ -0,0 +1,13 @@
+//
+//  TrackerTests.swift
+//  TrackerTests
+//
+//  Created by Pavel Belenkow on 07.08.2023.
+//
+
+import XCTest
+import SnapshotTesting
+@testable import Tracker
+
+final class TrackerTests: XCTestCase {
+}
